cd88efdb0fceff247db124e65e8c487c66c6d877
icesheep <380131090@qq.com>
2019-01-11 15:14:21 +0800

    去掉logo

--- src/pages/Live/singer/Ready.js
+++ src/pages/Live/singer/Ready.js
@@ -8,7 +8,7 @@ import IntroImg from '../../../assets/singer/intro.png';
 import PlayImg from '../../../assets/singer/play.png';
 import PauseImg from '../../../assets/singer/pause.png';
 import bg1 from '../../../assets/singer/bg1.png';
-import fmImg from '../../../assets/singer/fm.png';
+// import fmImg from '../../../assets/singer/fm.png';
 
 class Ready extends Component {
   constructor(props) {
@@ -162,7 +162,7 @@ class Ready extends Component {
         <img className={styles.imgBg} src={banner_images||bg1} />  
         <div className={styles.mask} />
         <div className={styles.div1}>
-          <img src={fmImg} className={styles.fmlogo} />
+          {/* <img src={fmImg} className={styles.fmlogo} /> */}
           <div className={styles.item1}>{banner_title}</div>
           <div className={styles.item2}>即将呈现</div>
           <div className={styles.item3}>
