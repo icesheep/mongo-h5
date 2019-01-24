5ff6d654415b7784677b614906e0348211faff24
zhoulv@himango.cn <zhoulv@himango.cn>
2019-01-24 09:43:47 +0800

    fix domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,21 @@
 import { stringify } from 'qs';
 import request from '@/utils/request';
 
+const URL = "dev-fm.tingdao.com";
 export async function queryList(params) {
-  return request('https://fm.tingdao.com/thridpartyapi/live/proxy?c=7004&v=1.0&sid=0000', {
+  return request('https://'+URL+'/thridpartyapi/live/proxy?c=7004&v=1.0&sid=0000', {
     method: 'POST',
     body: params,
   });
 }
 export async function querySinger(params) {
-  return request('https://fm.tingdao.com/thridpartyapi/h5/get_singer_page_info', {
+  return request('https://'+URL+'/thridpartyapi/h5/get_singer_page_info', {
     method: 'POST',
     body: params,
   });
 }
 export async function queryZhifou(params) {
-  return request('https://fm.tingdao.com/thridpartyapi/h5/get_zhifou_themelist', {
+  return request('https://'+URL+'/thridpartyapi/h5/get_zhifou_themelist', {
     method: 'POST',
     body: params,
   });
